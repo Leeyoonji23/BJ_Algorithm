ca078b078bd5df816463082c5d4b8d4bea2dd48b
yoonji Lee <90778858+Leeyoonji23@users.noreply.github.com>
2024-11-14 16:13:30 +0900

    [Silver V] Title: 영화감독 숌, Time: 388 ms, Memory: 23280 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+ 
+const input = fs.readFileSync("./dev/stdin").toString().trim().split("\n");
+ 
+const N = parseInt(input);
+ 
+let count = 0; // 종말의 수 개수
+let num = 0; // 현재 검사 중인 수
+ 
+// N보다 작을 때 까지 종말의 수 찾기
+// num을 1씩 증가시키면서 num을 문자열로 변환했을 때 '666'이 포함되어있다면 count 증가시키기
+while (count < N) {
+  num++;
+  if (String(num).includes("666")) {
+    count++;
+  }
+}
+console.log(num);
