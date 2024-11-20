1f2a215ba2f5ea12bedee0a87503ad6946233570
yoonji Lee <90778858+Leeyoonji23@users.noreply.github.com>
2024-11-21 04:14:12 +0900

    [Gold IV] Title: 뮤탈리스크, Time: 176 ms, Memory: 15616 KB -BaekjoonHub

---
+++ 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.js
@@ -0,0 +1,47 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+const N = Number(input[0]);
+const SCV = input[1].split(" ").map(Number);
+
+if (N == 2) {
+  SCV.push(0);
+} else if (N == 1) {
+  SCV.push(0);
+  SCV.push(0);
+}
+
+const dx = [9, 3, 9, 3, 1, 1];
+const dy = [3, 9, 1, 1, 9, 3];
+const dz = [1, 1, 3, 9, 3, 9];
+let memo = Array.from(Array(61), () =>
+  Array.from(Array(61), () => Array(61).fill(Infinity))
+);
+
+function mutal(scv1, scv2, scv3) {
+  if (scv1 < 0) scv1 = 0;
+  if (scv2 < 0) scv2 = 0;
+  if (scv3 < 0) scv3 = 0;
+
+  if (scv1 == 0 && scv2 == 0 && scv3 == 0) {
+    return 0;
+  }
+
+  let ret = memo[scv1][scv2][scv3];
+
+  if (ret != Infinity) {
+    return ret;
+  }
+
+  for (let i = 0; i < 6; i++) {
+    ret = Math.min(ret, mutal(scv1 - dx[i], scv2 - dy[i], scv3 - dz[i]) + 1);
+  }
+  memo[scv1][scv2][scv3] = ret;
+  return ret;
+}
+
+const answer = mutal(SCV[0], SCV[1], SCV[2]);
+console.log(answer);
