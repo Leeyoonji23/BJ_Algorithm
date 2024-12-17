97eb2d7e9de2dd2c0c90cae35d1dd64ae3f8b914
yoonji Lee <90778858+Leeyoonji23@users.noreply.github.com>
2024-12-18 03:52:13 +0900

    [Silver V] Title: 막대기, Time: 96 ms, Memory: 9612 KB -BaekjoonHub

---
+++ 백준/Silver/1094. 막대기/막대기.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+let X = +input;
+let answer = 0;
+
+while (X > 0) {
+  if (X % 2) answer++;
+  X = parseInt(X / 2);
+}
+
+answer = answer <= 0 ? 1 : answer;
+console.log(answer);
